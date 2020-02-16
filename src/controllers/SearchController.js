a60849067f42b7179259214a20d758aed902c38b
Diego Almeida <diegorh4@gmail.com>
2020-02-16 19:26:10 -0400

    Projeto inicial

---
+++ src/controllers/SearchController.js
@@ -0,0 +1,29 @@
+const Dev = require('../models/Dev');
+const parseStringArray = require('../utils/parseStringArray');
+
+module.exports = {
+
+    async index(req, res){
+        const { latitude, longitude, techs } = req.query;
+
+        const tecsArray = parseStringArray(techs);
+
+        const devs = await Dev.find({
+            techs: {
+                $in: tecsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude]
+                    },
+                    $maxDistance: 10000
+                },
+            }
+        });
+
+        return  res.json({ devs });
+    }
+
+}
